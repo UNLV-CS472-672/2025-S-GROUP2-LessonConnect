6a302fe770de9589a5fac69d11b593095f535195
FrankFurter123 <larosadiaz2015@gmail.com>
2025-02-26 21:40:14 -0800

    Added Copyright section and Customer Service quick links

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -242,7 +242,28 @@ export default function Home() {
                                 </li>
                             </ul>
                         </div>
+                        <div className="col-md-3 col-sm-6 mb-3">
+                            <h6 className="fw-semibold">Customer Service</h6>
+                            <ul className="list-unstyled">
+                                <li>
+                                    <a href="#" className="text-light text-decoration-none">FAQs</a>
+                                </li>
+                                <li>
+                                    <a href="#" className="text-light text-decoration-none"
+                                    >Support</a
+                                    >
+                                </li>
+                                <li>
+                                    <a href="#" className="text-light text-decoration-none"
+                                    >Contact Us</a
+                                    >
+                                </li>
+                            </ul>
+                        </div>
                     </div>
+                </div>
+                <div className="text-center mt-3">
+                    <p className="mb-0">&copy; 2025 LessonConnect. All Rights Reserved.</p>
                 </div>
             </footer>
         </>
