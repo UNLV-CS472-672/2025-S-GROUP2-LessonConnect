1370d06282767fb3e5e3377048879de39c27cdd3
Abdulrahman Alharbi <126299975+AbdulAlharbi@users.noreply.github.com>
2025-03-04 10:18:59 -0800

    App.jsx set up
    Added links for Login and SignUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Learn_more from "./components/Learn_more";
+import Login from "./Components/Login.jsx";
+import SignUp from "./Components/SignUp.jsx";
+import React from "react";
 
 function App() {
     return (
@@ -10,6 +13,11 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} /> {/* Fixed route path */}
                 <Route path="/learn_more" element={<Learn_more />} />
+                {/* 2. Login page (path="/login") */}
+                <Route path="/login" element={<Login />} />
+
+                {/* 3. Sign up page (path="/signup") */}
+                <Route path="/signup" element={<SignUp />} />
             </Routes>
         </Router>
     );
