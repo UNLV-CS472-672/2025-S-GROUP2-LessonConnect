947578ac4cad945fab08bf8c033dd67e063c6db3
aviendha-andrus <aviendha.aviendha@gmail.com>
2025-03-07 20:08:25 -0800

    Submissions model created, further testing and documentation needed

--- backend/apps/submissions/admin.py
+++ backend/apps/submissions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Submissions, FileSubmissions, QuizSubmissions, StudentQuizAnswers
+
+admin.site.register(Submissions)
+admin.site.register(FileSubmissions)
+admin.site.register(QuizSubmissions)
+admin.site.register(StudentQuizAnswers)
