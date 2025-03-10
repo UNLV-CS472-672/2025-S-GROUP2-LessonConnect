920c74ec2ce643a6038407c657d1072da4f8a02f
FrankFurter123 <larosadiaz2015@gmail.com>
2025-03-09 21:06:34 -0700

    Rename index.css to Home.css for Homepage styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './Styles/index.css' //Global style
+import './Styles/Home.css' //Global style
 import App from './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
