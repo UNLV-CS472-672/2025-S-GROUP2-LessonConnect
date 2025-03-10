32905637a1f9aea551cf4cc7c0d7490c99613b94
FrankFurter123 <larosadiaz2015@gmail.com>
2025-03-09 21:40:22 -0700

    Added Footer component with conditional rendering for specific page and removed Footer from About.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./Components/Header.jsx";
-import Home from "./components/Home";
-import About from "./components/About";
-import Learn_more from "./components/Learn_more";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Learn_more from "./components/Learn_more.jsx";
 import Login from "./Components/Login.jsx";
 import SignUp from "./Components/SignUp.jsx";
 import DateOfBirth from "./Components/DateOfBirth.jsx";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
     const location = useLocation();
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/SignUp" element={<SignUp />} />
                 <Route path="/dateofbirth" element={<DateOfBirth />} />
             </Routes>
+            {(location.pathname !== "/login" && location.pathname !== "/dateofbirth" && location.pathname !== "/SignUp") && <Footer />}
         </div>
     );
 }
--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -95,62 +95,6 @@ export default function About() {
                     </div>
                 </div>
             </section>
-
-            <footer className="bg-dark text-light py-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-3 col-sm-6 mb-3">
-                            <h6 className="fw-semibold">LessonConnect</h6>
-                            <ul className="list-unstyled">
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none"
-                                    >About Us</a
-                                    >
-                                </li>
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none"
-                                    >Learn More</a
-                                    >
-                                </li>
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none"></a>
-                                </li>
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none"
-                                    >Services</a
-                                    >
-                                </li>
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none"
-                                    >Schedule</a
-                                    >
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col-md-3 col-sm-6 mb-3">
-                            <h6 className="fw-semibold">Customer Service</h6>
-                            <ul className="list-unstyled">
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none">FAQs</a>
-                                </li>
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none"
-                                    >Support</a
-                                    >
-                                </li>
-                                <li>
-                                    <a href="#" className="text-light text-decoration-none"
-                                    >Contact Us</a
-                                    >
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className="text-center mt-3">
-                    <p className="mb-0">&copy; 2025 LessonConnect. All Rights Reserved.</p>
-                </div>
-            </footer>
         </>
     );
 }
