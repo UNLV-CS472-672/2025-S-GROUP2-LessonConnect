9a4e2eccaf4b01830cedb790a0a7f685337b26f4
Abdulrahman Alharbi <126299975+AbdulAlharbi@users.noreply.github.com>
2025-03-18 13:40:04 -0700

    Did a set up for booking from home page and made a link to Booking

---
+++ frontend/src/Components/Booking.jsx
@@ -0,0 +1,8 @@
+import {Link, NavLink} from "react-router-dom";
+
+import React, { useState } from "react";
+import "../Styles/Booking.css";
+
+export default function Booking() {
+//     will add code later 
+}
