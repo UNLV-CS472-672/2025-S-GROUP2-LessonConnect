dc82a075bf91f56598e3cf35901af7f6948fbac7
aviendha-andrus <aviendha.aviendha@gmail.com>
2025-03-22 22:00:50 -0700

    commit before rebase

--- backend/apps/submissions/urls.py
+++ backend/apps/submissions/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    SubmissionsViewSet,
-    FileSubmissionsViewSet,
-    QuizSubmissionsViewSet,
-    StudentQuizAnswersViewSet
-)
+from .views import SubmissionsViewSet, FileSubmissionsViewSet, QuizSubmissionsViewSet, StudentQuizAnswersViewSet
 
+# Create a router and register your viewsets
 router = DefaultRouter()
 router.register(r'submissions', SubmissionsViewSet)
 router.register(r'file-submissions', FileSubmissionsViewSet)
 router.register(r'quiz-submissions', QuizSubmissionsViewSet)
 router.register(r'student-quiz-answers', StudentQuizAnswersViewSet)
 
+# urlpatterns = [
+#     # Include the router URLs in your app's URLs
+#     path('', include(router.urls)),
+# ]
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
