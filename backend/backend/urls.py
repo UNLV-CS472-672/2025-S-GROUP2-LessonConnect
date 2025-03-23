a208a1f9ef084d06843f69a8cbc371fd5df7930c
aviendha-andrus <aviendha.aviendha@gmail.com>
2025-03-22 18:41:34 -0700

    debugging

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
   path('uploads/', include('apps.uploads.urls')),
   path('search/', include('apps.search.urls')),
   path('bookings/', include('apps.bookings.urls')),
+  path('submissions/', include('apps.submissions.urls')),
   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
