9ed515f30e4d9287fd26e898aaf4f79b26fe616b
asterid-leaper <asteridleaper@gmail.com>
2025-03-23 13:01:54 -0700

    Small syntax/style fixes for CI approval

--- frontend/src/Components/Booking.jsx
+++ frontend/src/Components/Booking.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css"; // npm install react-calendar
 import "../Styles/Booking.css";
