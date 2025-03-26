b7d37b3af31ab7ccc676f02b3b8349090e92fb2b
SameerIssa <96142685+SameerIssa@users.noreply.github.com>
2025-03-26 15:19:32 -0700

    fixed side panel errors, fixed button bar scaling

--- frontend/src/Components/VideoCall.jsx
+++ frontend/src/Components/VideoCall.jsx
@@ -9,17 +9,18 @@ const VideoCall = () => {
     return (
         <>
             <div className="Videocall-page-container">
-                {/* Side Panel */}
-                <div className="side-panel">
-                    <h2>Menu</h2>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/profile">Profile</a></li>
-                        <li><a href="/settings">Settings</a></li>
-                        <li><a href="/Whiteboard">Whiteboard</a></li>
-                    </ul>
-                </div>
+
                 <div className="container-fluid video-call-container">
+                    {/* Side Panel */}
+                    <div className="side-panel">
+                        <h2>Menu</h2>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/profile">Profile</a></li>
+                            <li><a href="/settings">Settings</a></li>
+                            <li><a href="/Whiteboard">Whiteboard</a></li>
+                        </ul>
+                    </div>
                     <div className="video-container">
                         {/* Remote Video */}
                         <video ref={remoteVideoRef} className="remote-video" autoPlay></video>
@@ -31,7 +32,7 @@ const VideoCall = () => {
                             <button className="btn btn-outline-danger">Switch Cameras</button>
                             <button className="btn btn-outline-danger">Mute</button>
                             <button className="btn btn-outline-danger">Share Screen</button>
-                            <button className="btn btn-outline-danger">Share Screen</button>
+                            <button className="btn btn-outline-danger">Settings</button>
                             <button className="btn btn-danger">End Call</button>
                         </div>
                     </div>
