488aa9dcde83adc4cfc219bbec1cb3b9e93de48f
Allison Kameda <kameda@unlv.nevada.edu>
2025-03-26 11:34:52 -0700

    Updated backend/urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
   path('bookings/', include('apps.bookings.urls')),
   path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+  path('pomodoro/', include('apps.pomodoro.urls')),
 ]
