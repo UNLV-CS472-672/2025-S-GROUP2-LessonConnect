2194c97c79f896f2007b3fec7d7ed428b431bd53
SameerIssa <96142685+SameerIssa@users.noreply.github.com>
2025-03-28 09:46:27 -0700

    created initial files for profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ import Schedule from "./Components/Schedule.jsx";
 import Chat from "./Components/Chat.jsx";
 import Footer from "./Components/Footer.jsx";
 import Booking from "./Components/Booking.jsx";
+import ProfilePage from "./Components/ProfilePage.jsx";
 
 function App() {
     const location = useLocation();
@@ -44,6 +45,7 @@ function App() {
                 <Route path="/pomodoro" element={<Pomodoro />} />
                 <Route path="/booking" element={<Booking />} />
                 <Route path="/chat" element={<Chat />} />
+                <Route path="/profile" element={<ProfilePage />} />
             </Routes>
             {(location.pathname !== "/login" && location.pathname !== "/dateofbirth" && location.pathname !== "/SignUp") && <Footer />}
         </div>
---
+++ frontend/src/Components/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import "../Styles/ProfilePage.css"
+
+export default function ProfilePage() {
+    return (
+        <>
+            <div className="profile-page-container">
+
+
+            </div>
+        </>
+    );
+}
