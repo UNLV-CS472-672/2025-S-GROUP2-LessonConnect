ff63d2ef246ad282f3a35d04fe99ca65694e41b1
Abdulrahman Alharbi <126299975+AbdulAlharbi@users.noreply.github.com>
2025-03-29 21:31:31 -0700

    Starter code for .jsx

--- frontend/src/Components/Calendar.jsx
+++ frontend/src/Components/Calendar.jsx
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 import '../Styles/Calendar.css'
-export default function Calendar() {
-    return (
-        <>
-            {/*will add code here later */}
-        </>
-    );
+import { FaPlus, FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+function LessonCalendar() {
+    // ---------------------------
+    // COMPONENT RENDER
+    // ---------------------------
+    // return (
+    //
+    // );
+    <>
+        {/*will add code here later */}
+    </>
 }
+export default LessonCalendar;
