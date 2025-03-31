f883dcecf5adf18ffde727fa0e02d3ce73455f7f
Allison Kameda <kameda@unlv.nevada.edu>
2025-03-31 13:10:27 -0700

    Update backend/apps/pomodoro/views.py
    Co-authored-by: Ethan Zambrano <161177620+EthanZ23@users.noreply.github.com>

--- backend/apps/pomodoro/views.py
+++ backend/apps/pomodoro/views.py
@@ -17,7 +17,10 @@ class StartPomodoroSession(APIView):
 
 class PausePomodoroSession(APIView):
     def post(self, request, id):
-        session = PomodoroSession.objects.get(id=id)
+        session = PomodoroSession.objects.filter(id=id).first()
+        if not session:
+            return Response({"error": "Pomodoro session not found"}, status=status.HTTP_404_NOT_FOUND)
+        
         session.pause()
         return Response({"message": "Pomodoro session paused"}, status=status.HTTP_200_OK)
 
