a27161dd3da2bf55eef4bf2b6f39c227d744558a
asterid-leaper <asteridleaper@gmail.com>
2025-03-31 17:27:03 -0700

    Adding comments + More Flexible

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -16,30 +16,34 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 # https://medium.com/@farad.dev/how-to-build-a-real-time-chat-app-using-django-channels-2ba2621ea972
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = 'chat_room'
-        self.room_group_name = 'chat_%s' % self.room_name
-        await self.channel_layer.group_add(
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"] # Extracts the room_name from the URL.
+        self.room_group_name = f'chat_{self.room_name}'
+        await self.channel_layer.group_add( # adds the WebSocket connection (client) to a channel group
             self.room_group_name,
             self.channel_name
         )
         await self.accept()
+
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+# This function receive messages from WebSocket.
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        await self.channel_layer.group_send(
+        await self.channel_layer.group_send( # sends message to all users in a channel group
             self.room_group_name,
             {
-                'type': 'chat_message',
+                'type': 'chat_message', # Calls chat_message()
                 'message': message
             }
         )
+
     async def chat_message(self, event):
         message = event['message']
+        #send message of sender to websocket
         await self.send(text_data=json.dumps({
             'message': message
         }))
--- backend/apps/chat/routing.py
+++ backend/apps/chat/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/apps/chat/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    #re_path(r'ws/apps/chat/$', consumers.ChatConsumer.as_asgi()),
     #re_path(r'ws/apps/chat/practice/$', consumers.PracticeConsumer.as_asgi()),
 ]
