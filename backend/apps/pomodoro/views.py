9f2ea30ec594616a56ce705502e15f12a90033ec
Allison Kameda <kameda@unlv.nevada.edu>
2025-04-01 20:05:01 -0700

    Update backend/apps/pomodoro/views.py
    Co-authored-by: Ethan Zambrano <161177620+EthanZ23@users.noreply.github.com>

--- backend/apps/pomodoro/views.py
+++ backend/apps/pomodoro/views.py
@@ -53,7 +53,10 @@ class CompletePomodoroSession(APIView):
 
 class GetPomodoroSessionStatus(APIView):
     def get(self, request, id):
-        session = PomodoroSession.objects.get(id=id)
+        session = PomodoroSession.objects.filter(id=id).first()
+        if not session:
+            return Response({"error": "Pomodoro session not found"}, status=status.HTTP_404_NOT_FOUND)
+            
         serializer = PomodoroSessionSerializer(session)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
