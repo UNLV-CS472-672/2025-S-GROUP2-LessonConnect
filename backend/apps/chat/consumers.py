392aa0bd9e5d7c7c54bcd720048c80f1395ab5ba
asterid-leaper <asteridleaper@gmail.com>
2025-04-01 19:50:33 -0700

    wip

--- backend/apps/chat/consumers.py
+++ backend/apps/chat/consumers.py
@@ -3,36 +3,50 @@
 # and entered ws://localhost:8000/ws/apps/chat/practice/
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-
-#class PracticeConsumer(AsyncJsonWebsocketConsumer):
-#      async def connect(self):
-#           await self.accept()
-#
-#      async def receive(self, text_data=None, bytes_data=None, **kwargs):
-#            if text_data == 'PING':
-#                # Send back a response
-#                await self.send(text_data='{"message": "PONG"}')
+from asgiref.sync import sync_to_async
+from .models import Message, Chat
 
 # https://medium.com/@farad.dev/how-to-build-a-real-time-chat-app-using-django-channels-2ba2621ea972
 class ChatConsumer(AsyncWebsocketConsumer):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+        self.room_group_name = None
+        self.room = None
+        self.user = None
+
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"] # Extracts the room_name from the URL.
+        user = self.scope["user"]  # Get the user from the scope
+
+        if not user.is_authenticated:
+            await self.close()  # Close the connection if the user is not authenticated
+            return
+
+        self.user = user
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]  # Extracts the room_name from the URL
         self.room_group_name = f'chat_{self.room_name}'
-        await self.channel_layer.group_add( # adds the WebSocket connection (client) to a channel group
+
+        await self.channel_layer.group_add(  # Adds the WebSocket connection (client) to a channel group
             self.room_group_name,
             self.channel_name
         )
         await self.accept()
+
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
-# This function receive messages from WebSocket.
+# This function receive messages from WebSocket. 10:58
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+
+        # Create a new message object
+        await self.save_message(self.user, self.room_name, message)
+
         await self.channel_layer.group_send( # sends message to all users in a channel group
             self.room_group_name,
             {
@@ -47,3 +61,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': message
         }))
+
+    @sync_to_async
+    def save_message(self, room_name, user, content):
+        try:
+            chat = Chat.objects.get(name = room_name)
+            Message.objects.create(content=content, chat = chat, sender = user) # Creates and saves message
+        except (Chat.DoesNotExist):
+            print(f"Chat room '{room_name}' not found. Message not saved.")
