762f4406af1d1d0e71637523c28a7bc899f8b58b
Dorian Akhavan <91112526+omgdory@users.noreply.github.com>
2025-04-19 17:11:49 -0700

    Update backend/apps/chat/urls.py
    Co-authored-by: Ethan Zambrano <161177620+EthanZ23@users.noreply.github.com>

--- backend/apps/chat/urls.py
+++ backend/apps/chat/urls.py
@@ -11,7 +11,7 @@ router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
   path('', include(router.urls)),
-  path('', chat_list, name='chat_list'),
+  path('legacy/', chat_list, name='chat_list'),
   path('<int:chat_id>/', chat_detail, name='chat_detail'),
   path('send/', select_send_message, name='select_send_message'),
   path('send/<int:user_id>/', send_message, name='send_message'),
