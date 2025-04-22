d1c46c1ddcbab5be2e804ddb1a961fa290b96155
SameerIssa <96142685+SameerIssa@users.noreply.github.com>
2025-04-22 13:54:35 -0700

    Removed error causing code

--- frontend/src/Components/EditProfile.jsx
+++ frontend/src/Components/EditProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaCamera, FaEye, FaLock, FaBell } from 'react-icons/fa';
 import '../Styles/EditProfile.css';
