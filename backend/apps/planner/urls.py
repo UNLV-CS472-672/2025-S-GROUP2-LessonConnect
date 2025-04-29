7be4ac58b482836c22adad94942658726c4827f8
Ethan Z <161177620+EthanZ23@users.noreply.github.com>
2025-04-29 10:58:33 -0700

    Implemented a DRF Serializer for UnscheduledTask model. Added API Views. Setup URLS.

--- backend/apps/planner/urls.py
+++ backend/apps/planner/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CalendarEventViewSet
+from .views import CalendarEventViewSet, UnscheduledTaskViewSet
 
 # Create a router and register viewsets
 router = DefaultRouter()
 router.register(r'calendar', CalendarEventViewSet)
+router.register(r'unscheduled-tasks', UnscheduledTaskViewSet, basename='unscheduledtask')
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/', include(router.urls))
 ]
