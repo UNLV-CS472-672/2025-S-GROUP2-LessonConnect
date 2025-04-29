e685f30a1f103b24627d940094bbeba6f9df8142
Ethan Z <161177620+EthanZ23@users.noreply.github.com>
2025-04-29 10:04:59 -0700

    Unscheduled task model created and it's serializer

--- backend/apps/planner/serializers.py
+++ backend/apps/planner/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
-from .models import CalendarEvent
+
+
+from .models import CalendarEvent, UnscheduledTask
 
 class CalendarEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = CalendarEvent
         fields = '__all__'
 
+
+
+
+
+
+
+
+class UnscheduledTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UnscheduledTask
+        fields = [
+            'id',
+            'title',
+            'description',
+            'due_date',
+            'assigned_assignment',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = ['created_at', 'updated_at']
