893240003a59fb3f1eef84755fa8ce703fcccef3
asterid-leaper <asteridleaper@gmail.com>
2025-05-06 15:03:56 -0700

    for deployment redirects

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // Ensures correct routing
+  build: {
+    outDir: 'dist' // Default, but explicit is better
+  },
   server: {
     fs: {
       allow: ['../'], // Allows access to node_modules outside the root
     }
   }
-})
+});
