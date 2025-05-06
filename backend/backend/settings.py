162749edc56180bb3cb73139dc300306efcdd3f7
asterid-leaper <asteridleaper@gmail.com>
2025-05-06 16:09:51 -0700

    Revert "Update settings.py"
    This reverts commit 9cdbf554e8244579c8666b8dbf3aedced5b2abc0.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,7 +133,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default=os.getenv('DATABASE_URL'),
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
